4df88b863824de9f85755f8be6aa5212fd019973
Bob Bailey <bbailey@brightcove.com>
2023-01-12 16:39:16 -0500

    update install submodules file

--- install-submodules.sh
+++ install-submodules.sh
@@ -5,10 +5,36 @@ cd ../_includes
 git submodule add https://github.com/BrightcoveLearning/s-jekyll-page-includes
 git submodule add https://github.com/BrightcoveLearning/s-support
 git submodule add https://github.com/BrightcoveLearning/s-template-page-includes
+git submodule add https://github.com/BrightcoveLearning/s-admin
+git submodule add https://github.com/BrightcoveLearning/s-cloudplayout
+git submodule add https://github.com/BrightcoveLearning/s-social
+git submodule add https://github.com/BrightcoveLearning/s-players
+git submodule add https://github.com/BrightcoveLearning/s-publish
+git submodule add https://github.com/BrightcoveLearning/s-general
+git submodule add https://github.com/BrightcoveLearning/s-support
+git submodule add https://github.com/BrightcoveLearning/s-campaign
+git submodule add https://github.com/BrightcoveLearning/s-training
+git submodule add https://github.com/BrightcoveLearning/s-live
+git submodule add https://github.com/BrightcoveLearning/s-web-conferencing
+git submodule add https://github.com/BrightcoveLearning/s-brightcove-interactivity
+git submodule add https://github.com/BrightcoveLearning/s-template-page-includes
 cd ..
 cd assets/images
 git submodule add https://github.com/BrightcoveLearning/s-support-img
 git submodule add https://github.com/BrightcoveLearning/s-feather-img
+git submodule add https://github.com/BrightcoveLearning/s-admin-img
+git submodule add https://github.com/BrightcoveLearning/s-cloudplayout-img
+git submodule add https://github.com/BrightcoveLearning/s-social-img
+git submodule add https://github.com/BrightcoveLearning/s-players-img
+git submodule add https://github.com/BrightcoveLearning/s-publish-img
+git submodule add https://github.com/BrightcoveLearning/s-general-img
+git submodule add https://github.com/BrightcoveLearning/s-support-img
+git submodule add https://github.com/BrightcoveLearning/s-feather-img
+git submodule add https://github.com/BrightcoveLearning/s-campaign-img
+git submodule add https://github.com/BrightcoveLearning/s-training-img
+git submodule add https://github.com/BrightcoveLearning/s-live-img
+git submodule add https://github.com/BrightcoveLearning/s-web-conferencing-img
+git submodule add https://github.com/BrightcoveLearning/s-brightcove-interactivity-img
 cd ..
 echo 'Adding site assets'
 git submodule add https://github.com/BrightcoveLearning/s-site-assets-favicons
